76dd9949359586d5205e72910c9af268437dece8
Gerimantas <g.pukys@gmail.com>
2020-05-20 14:30:54 +0300

    replace

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wix-task'`, () => {
+  it(`should have as title 'some-task'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('wix-task');
+    expect(app.title).toEqual('some-task');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('wix-task app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('some-task app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wix-task';
+  title = 'some-task';
 }
